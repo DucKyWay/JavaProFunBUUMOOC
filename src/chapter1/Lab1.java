997e1f0c29c3fbf47caee5c47aeeb705d872390f
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2022-11-06 22:25:56 +0700

    catch up

---
+++ src/chapter1/Lab1.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Name: Suphawinee Chatasawapreeda");
+		System.out.println("Faculty: Science Mathematics Technology room");
+		System.out.println("School: Sarawittaya School");
+	}
+
+}
---
+++ src/chapter2/Lab2_1.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class Lab2_1 {
+
+	public static void main(String[] args) {
+		byte value1 = 5;
+		short value2 = 1500;
+		float value3 = 10.123456f;
+		double value4 = 5.123456789;
+		char value5 = 'A';
+		String value6 = "Burapha";
+		boolean value7 = true;
+
+	}
+
+}
